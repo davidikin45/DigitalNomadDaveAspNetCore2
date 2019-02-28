0647eb190342d582c3d576ab3f7a0d7b90e6f357
David Ikin <davidikin45@gmail.com>
2019-02-28 16:05:28 +0000

    Environment Variables

--- src/AspNetCore.Testing/EnvironmentVariablesFixture.cs
+++ src/AspNetCore.Testing/EnvironmentVariablesFixture.cs
@@ -10,23 +10,26 @@ namespace AspNetCore.Testing
     {
         public EnvironmentVariablesFixture()
         {
-            using (var file = File.OpenText("Properties\\launchSettings.json"))
+            if(File.Exists("Properties\\launchSettings.json"))
             {
-                var reader = new JsonTextReader(file);
-                var jObject = JObject.Load(reader);
-
-                var variables = jObject
-                    .GetValue("profiles")
-                    //select a proper profile here
-                    .SelectMany(profiles => profiles.Children())
-                    .SelectMany(profile => profile.Children<JProperty>())
-                    .Where(prop => prop.Name == "environmentVariables")
-                    .SelectMany(prop => prop.Value.Children<JProperty>())
-                    .ToList();
-
-                foreach (var variable in variables)
+                using (var file = File.OpenText("Properties\\launchSettings.json"))
                 {
-                    Environment.SetEnvironmentVariable(variable.Name, variable.Value.ToString());
+                    var reader = new JsonTextReader(file);
+                    var jObject = JObject.Load(reader);
+
+                    var variables = jObject
+                        .GetValue("profiles")
+                        //select a proper profile here
+                        .SelectMany(profiles => profiles.Children())
+                        .SelectMany(profile => profile.Children<JProperty>())
+                        .Where(prop => prop.Name == "environmentVariables")
+                        .SelectMany(prop => prop.Value.Children<JProperty>())
+                        .ToList();
+
+                    foreach (var variable in variables)
+                    {
+                        Environment.SetEnvironmentVariable(variable.Name, variable.Value.ToString());
+                    }
                 }
             }
         }
